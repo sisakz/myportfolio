318aba2abe24224f1b13e524f09cc670103937a2
Zoltan Sisak <zoltan.sisak@gmail.com>
2022-06-21 00:43:34 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [value, setValue] = useState(null);
+  useEffect(() => {
+    // GET request using fetch inside useEffect React hook
+    fetch(
+      "https://shrouded-sea-88824.herokuapp.com/https://www.bamosz.hu/alapoldal?isin=HU0000702758"
+    )
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setValue(getCurrentValueOfMyPortfolio(data));
+      });
+  }, []);
+  return <div className="App">{value}</div>;
 }
 
 export default App;
+
+const getCurrentValueOfMyPortfolio = (htmlInput) => {
+  const inputwlb = htmlInput.replace(/\r?\n|\r/g, "");
+  const inputwlb2 = inputwlb.replace(/\s+/g, "");
+
+  const start = inputwlb2.indexOf('Árfolyam:</td><tdclass="data">');
+  const rate = Number(
+    inputwlb2.substring(start + 30, start + 36).replace(",", ".")
+  );
+  const packet = 1820543;
+  return Math.round(packet * rate).toLocaleString("hu-HU", {
+    style: "currency",
+    currency: "HUF",
+  });
+};
