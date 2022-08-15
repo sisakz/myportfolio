47da258d762a5b7049d21877fac2dc046569c0b6
Zoltan <zoltan.sisak@gmail.com>
2022-08-15 20:22:15 +0100

    spinner jokamat

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+
 
 const investedAmount = 7000000
 const startOfInvestingDate = new Date("05/30/2022");
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
 
 function App() {
   const [value, setValue] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [color, setColor] = useState("#ffffff");
   useEffect(() => {
     // GET request using fetch inside useEffect React hook
     fetch(
-      "https://shrouded-sea-88824.herokuapp.com/https://www.bamosz.hu/alapoldal?isin=HU0000702758"
+      //"https://shrouded-sea-88824.herokuapp.com/https://www.bamosz.hu/alapoldal?isin=HU0000702758"
+      "https://shrouded-sea-88824.herokuapp.com/http://www.jokamat.hu/raiffeisen-kamat-premium-rovid-kotv.html"
     )
       .then((response) => response.text())
       .then((data) => {
-        console.log(data);
+        setLoading(false)
+        //console.log(data);
         setValue(getCurrentValueOfMyPortfolio(data));
       });
   }, []);
-  return <div className="App">{value}</div>;
-}
+  return (
+    <div className="App">
+      <ClipLoader color={color} loading={loading} cssOverride={override} size={150} />
+      <h1>Raiffeisen Kamat Prémium Rövid Kötvény Alap</h1>
+      {value}
+    </div>
+    )}
 
 export default App;
 
 const getCurrentValueOfMyPortfolio = (htmlInput) => {
   const inputwlb = htmlInput.replace(/\r?\n|\r/g, "");
   const inputwlb2 = inputwlb.replace(/\s+/g, "");
-
-  const start = inputwlb2.indexOf('Árfolyam:</td><tdclass="data">');
+  console.log(inputwlb2)
+  //const start = inputwlb2.indexOf('Árfolyam:</td><tdclass="data">');
+  const start = inputwlb2.indexOf('<spanstyle="font-size:110%;text-decoration:none;">')
   const baseRate = 3.8426
   const rate = Number(
-    inputwlb2.substring(start + 30, start + 36).replace(",", ".")
+    inputwlb2.substring(start + 50, start + 58).replace(",", ".")
   );
+  console.log(inputwlb2.substring(start + 50, start + 58).replace(",", "."))
+  const rate2 = 3.902452
   
   const ActualDate = new Date();
   const investingPeriodInTime = ActualDate.getTime() - startOfInvestingDate.getTime();
@@ -64,7 +85,7 @@ const getCurrentValueOfMyPortfolio = (htmlInput) => {
 
   return (
     <>
-      <h1>Raiffeisen Kamat Prémium Rövid Kötvény Alap</h1>
+
       <div>Befektetett összeg: {investedAmountToString}</div>
       <div>Eltelt napok száma a befektetés kezdete óta: {investingPeriodInDay}</div>
       <div>Éves névleges hozam: {yearlyRateBenefit}%</div>
@@ -84,7 +105,7 @@ const getCurrentValueOfMyPortfolio = (htmlInput) => {
       <div>Éves nettó hozam: {yearlyRateBenefitAfterTaxAndFees} %</div>
       <br />
       <div>
-        <a href="https://www.bamosz.hu/alapoldal?isin=HU0000702758">Bamosz</a>
+        <a href="http://www.jokamat.hu/raiffeisen-kamat-premium-rovid-kotv.html">Jokamat</a>
       </div>
     </>
  )
